159bdb299959f9b335c9a090b565050c9ebeb56e
alpesh301094 <alpesh301094@gmail.com>
2023-03-13 19:28:06 +0530

    changes

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
 const postsRoute = require('./routes/posts');
 app.use(bodyParser.json());
+app.use(cors());
 app.use('/posts', postsRoute);
-
 module.exports = app;
